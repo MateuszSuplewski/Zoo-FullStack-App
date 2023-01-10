d25fe0052e6bff08d2ca9fdcde5f39110346ee3e
MateuszSuplewski <matx3582@gmail.com>
2023-01-10 20:08:01 +0100

    Init CRA + dependencies

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { Routes, Route } from 'react-router-dom'
+import Login from './pages/Login';
+import Register from './pages/Register';
+
+function App() {
+  return(
+    <div>
+    </div>
+  )
+}
+
+export default App;
